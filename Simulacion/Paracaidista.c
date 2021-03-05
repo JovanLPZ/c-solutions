77d74dbe96e4fc57237ce9ee84c12d1eeee7fe5a
JovanLPZ <sjx.antrax@gmail.com>
2021-03-05 00:13:03 -0700

    Version de entrega

--- Simulacion/Paracaidista.c
+++ Simulacion/Paracaidista.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include "pbPlots.h"
+#include "supportLib.h"
 
 #define RESISTENCIA_AIRE 12.5f
 #define GRAVEDAD 9.8f
@@ -9,12 +11,14 @@ int main(void)
 {
     float masa, velocidad, velocidad_anterior;
 
+    double x[60], y[100];
+
     printf("\nIngrese la masa del paracaidista: ");
     scanf("%f", &masa);
 
     printf("\nTiempo(s)\tVelocidad(m/s)\n");
 
-    int t = 2;
+    int t = 0, j = 0;
     do
     {
         velocidad = ((GRAVEDAD * masa) / RESISTENCIA_AIRE) * (1 - pow(E, -(RESISTENCIA_AIRE / masa) * t));
@@ -22,11 +26,20 @@ int main(void)
 
         printf("%d\t\t%.4f\n", t, velocidad);
 
-        t+=2;
+        x[j] = t;
+        y[j] = velocidad;
+        t+=2, ++j;
+
 
     } while ((velocidad - velocidad_anterior) > 0.000001f );
     
-    
+    RGBABitmapImageReference *imageRef = CreateRGBABitmapImageReference();
+
+    DrawScatterPlot(imageRef, 600, 400, x, 38, y, 38);
+
+    size_t length;
+    double *pngData = ConvertToPNG(&length, imageRef->image);
+    WriteToFile(pngData, length, "grafica.png");
     
 
     return 0;
